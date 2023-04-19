b7fecbd08770ff9c3d595c5d319275ece33ab63d
mohamedanas00 <117121756+mohamedanas00@users.noreply.github.com>
2023-04-19 17:53:11 +0200

    Final

--- Routes/PostRouter.js
+++ Routes/PostRouter.js
@@ -1,87 +1,110 @@
-const express =require("express");
-const route =express.Router();
+const express = require("express");
+const route = express.Router();
 const post = require("../Modules/Post");
 const user = require("../Modules/Users");
 const Post = require("../Modules/Post");
 
 
-route.get("/",async(req,res)=>{
-    const POST =await post.findAll();
-    res.send(POST);
+route.get("/:Id", async (req, res) => {
+  try {
+    const Post = await post.findOne({
+      where: {
+        id: req.params.Id
+      },
+      attributes: ['userId']
+    });
+    let i = Post.userId;
+    const User = await user.findOne({
+      where: {
+        id: i
+      },
+      attributes: ['role']
+    });
+    if (User.role === 'admin') {
+      const GetPost = await post.findByPk(req.params.Id);
+      await GetPost.update(req.body);
+      res.send(GetPost);
+    } else {
+      res.status(404).send("YOU DONT HAVE PERMISION TO ACCESS THIS FEATURE ⛔")
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Unable to Updat post' });
+  }
 })
 
 route.post('/', async (req, res) => {
-    const { userId, title,body } = req.body;
-    try {
-      const User=await user.findOne({
-        where: {
-          id: userId
-        },
-        attributes:['role']
-      });
-       if(User.role==='admin'){
-        const NewPost = await post.create({ userId, title, body });
-        res.status(201).send("POST ADD SUCCESSFULY");
-       }else{
-            res.status(404).send("YOU DONT HAVE PERMISION TO ACCESS THIS FEATURE ⛔");
-       }
-      
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Unable to create post' });
+  const { userId, title, body } = req.body;
+  try {
+    const User = await user.findOne({
+      where: {
+        id: userId
+      },
+      attributes: ['role']
+    });
+    if (User.role === 'admin') {
+      const NewPost = await post.create({ userId, title, body });
+      res.status(201).send("POST ADD SUCCESSFULY");
+    } else {
+      res.status(404).send("YOU DONT HAVE PERMISION TO ACCESS THIS FEATURE ⛔");
     }
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Unable to create post' });
+  }
 });
 
 route.put('/:Id', async (req, res) => {
-    try {
-      const Post=await post.findOne({
-        where: {
-          id:req.params.Id
-        },
-        attributes:['userId']
-      });
-      let i=Post.userId;
-      const User=await user.findOne({
-        where: {
-          id:i
-        },
-        attributes:['role']
-      });
-      if(User.role==='admin'){
-        const UpPost = await post.findByPk(req.params.Id);
-        await UpPost.update(req.body);
-        res.send("POST UPDATE SUCEESFULLL");
-       }else{
-            res.status(404).send("YOU DONT HAVE PERMISION TO ACCESS THIS FEATURE ⛔")
-       }  
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Unable to Updat post' });
+  try {
+    const Post = await post.findOne({
+      where: {
+        id: req.params.Id
+      },
+      attributes: ['userId']
+    });
+    let i = Post.userId;
+    const User = await user.findOne({
+      where: {
+        id: i
+      },
+      attributes: ['role']
+    });
+    if (User.role === 'admin') {
+      const UpPost = await post.findByPk(req.params.Id);
+      await UpPost.update(req.body);
+      res.send("POST UPDATE SUCEESFULLL");
+    } else {
+      res.status(404).send("YOU DONT HAVE PERMISION TO ACCESS THIS FEATURE ⛔")
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Unable to Updat post' });
+  }
 });
 
 route.delete('/:Id', async (req, res) => {
   try {
-    const Post=await post.findOne({
+    const Post = await post.findOne({
       where: {
-        id:req.params.Id
+        id: req.params.Id
       },
-      attributes:['userId']
+      attributes: ['userId']
     });
-    let i=Post.userId;
-    const User=await user.findOne({
+    let i = Post.userId;
+    const User = await user.findOne({
       where: {
-        id:i
+        id: i
       },
-      attributes:['role']
+      attributes: ['role']
     });
-    if(User.role==='admin'){
+    if (User.role === 'admin') {
       const dEPost = await post.findByPk(req.params.Id);
       await dEPost.destroy();
       res.send("POST DELETE SUCEESFULLL");
-     }else{
-          res.status(404).send("YOU DONT HAVE PERMISION TO ACCESS THIS FEATURE ⛔")
-     }  
+    } else {
+      res.status(404).send("YOU DONT HAVE PERMISION TO ACCESS THIS FEATURE ⛔")
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Unable to Updat post' });
