99945f1b3b6c3be674fb9db71f3f903d5b24cd6e
mohamedanas00 <117121756+mohamedanas00@users.noreply.github.com>
2023-04-19 04:24:01 +0200

    Add models->Users

---
+++ Models/Users.js
@@ -0,0 +1,23 @@
+const db =require("../Config/DataBase");
+const { DataTypes } =require("sequelize");
+
+module.exports =db.define(
+    "users",
+    {
+        Username:{
+           type:DataTypes.STRING(100),
+           allowNull:false,
+           unique: true,
+        },
+        password:{
+            type:DataTypes.STRING(255),
+            allowNull:false,
+        },
+        email:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique: true,
+        }
+    }
+);
+
