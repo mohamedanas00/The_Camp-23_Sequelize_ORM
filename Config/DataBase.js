274eaa529b2fba4d6046442f05071194b0526c2f
mohamedanas00 <117121756+mohamedanas00@users.noreply.github.com>
2023-04-19 08:23:46 +0200

    Update

--- Config/DataBase.js
+++ Config/DataBase.js
@@ -3,7 +3,7 @@ const sequelize = new Sequelize({
     host :"localhost",
     dialect:"mysql",
     username:"root",
-    database: " sequelize_orm",
+    database: "sequelize_orm",
     logging: console.log,
 });
 
