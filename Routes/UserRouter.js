cde89962445f77c07832d8521f87cca73066b647
mohamedanas00 <117121756+mohamedanas00@users.noreply.github.com>
2023-04-19 11:10:04 +0200

    Update

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -1,10 +1,11 @@
 const express =require("express");
 const route =express.Router();
 const user = require("../Modules/Users")
-// route.get("/",async(req,res)=>{
-//     const user =await User.findAll();
-//     res.send(user);
-// })
+
+route.get("/",async(req,res)=>{
+    const User =await user.findAll();
+    res.send(User);
+})
 
 route.post("/",async(req,res)=>{
     const { Username, password, email,role } = req.body;
@@ -16,4 +17,21 @@ route.post("/",async(req,res)=>{
       res.status(500).json({ error: 'Unable to create user' });
     }
 })
+
+route.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    try {
+      const UpUser = await user.findByPk(id);
+      if (!UpUser) {
+
+        return res.status(404).json({ error: 'User not found' });
+      }
+      await UpUser.destroy();
+      res.status(204).send( 'User Deleted Success' );
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Unable to delete user' });
+    }
+});
 module.exports = route;
